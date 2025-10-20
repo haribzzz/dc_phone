6c2c6f672249f4e94581319575fe870c1482e5f1
Harisema Amaya <haribzzz@gmail.com>
2025-10-19 21:14:23 -0600

    Actualizar server.js con conexión a SQL Server y API de productos

---
+++ server.js
@@ -0,0 +1,18 @@
+
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname)));
+
+//ruta principal
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// iniciar el servidor
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
